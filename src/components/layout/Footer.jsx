5c980f5c2c5c307afd67c324a803fa3423d33f1f
Youmni <youmni.malha@gmail.com>
2025-07-10 22:10:57 +0200

    Added cv download

--- src/components/layout/Footer.jsx
+++ src/components/layout/Footer.jsx
@@ -1,12 +1,23 @@
 import React from "react";
 import { FaLinkedin, FaGithub, FaEnvelope } from "react-icons/fa";
+import { FiDownload } from 'react-icons/fi';
 
 const Footer = () => {
   return (
     <footer className="bg-[#22223b] text-[#f2e9e4] py-6 mt-12 border-t border-[#4a4e69]">
       <div className="max-w-6xl mx-auto flex flex-col items-center text-center px-6 space-y-4">
-        <p className="text-lg font-semibold">&copy; {new Date().getFullYear()} Youmni Malha</p>
-        
+        <div className="flex flex-row items-center space-y-2 space-x-3">
+        <p className="text-lg font-semibold">
+          &copy; {new Date().getFullYear()} Youmni Malha
+        </p>
+        <a href="/public/cv.pdf" download="Youmni_Malha_CV.pdf" className="no-underline">
+          <FiDownload
+            size={24}
+            className="cursor-pointer text-[#f2e9e4] hover:text-[#288e9c] transition-colors"
+          />
+        </a>
+        </div>
+
         <div className="flex space-x-6">
           <a
             href="https://www.linkedin.com/in/youmni-malha-0a54972a3/"
